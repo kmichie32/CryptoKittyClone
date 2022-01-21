7f7a78b4f71d6e397f0323bacb5fac29ce982264
Kevin Michie <kmichie32@gmail.com>
2022-01-21 11:46:10 -0600

    Added kitty creation and retrieval

--- contracts/kittyContract.sol
+++ contracts/kittyContract.sol
@@ -2,14 +2,23 @@ pragma solidity >=0.5.10;
 
 import "./IERC721.sol";
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract kittyContract is IERC721 {
+contract kittyContract is IERC721, Ownable {
 
     using SafeMath for uint256;
 
+    event Birth(
+                address owner, 
+                uint256 kittenId,
+                uint256 mumId, 
+                uint256 dadId, 
+                uint256 genes
+            );
     string public constant token = "KevinKitties";
     string public constant symbolOfToken = "KK";
+    uint256 public constant CREATION_LIMIT_GEN0 = 10;
     mapping(address => uint256) ownershipTokenCount;
     mapping(uint256 => address) public kittyIndexToOwner;
 
@@ -23,6 +32,57 @@ contract kittyContract is IERC721 {
 
     Kitty[] kitties;
     
+    uint256 gen0Counter;
+
+    function getKitty(uint256 _kittenId) public returns(
+        uint256 genes, 
+        uint256 birthTime,
+        uint256 mumId,
+        uint256 dadId,
+        uint256 generation,
+        address owner) {
+            require(_kittenId < kitties.length, "Kitty not found");
+
+            return (kitties[_kittenId].genes, 
+            uint256(kitties[_kittenId].birthTime),
+            uint256(kitties[_kittenId].mumId),
+            uint256(kitties[_kittenId].dadId),
+            uint256(kitties[_kittenId].generation),
+            ownerOf(_kittenId)
+            );
+        }
+
+    function createKittyGen0(uint256 _genes) public onlyOwner returns (uint256){
+        require(gen0Counter < CREATION_LIMIT_GEN0);
+
+        gen0Counter = gen0Counter.add(1);
+
+        return _createKitty(0, 0, 0, _genes, msg.sender);
+    }
+
+    function _createKitty(
+        uint256 _mumId, 
+        uint256 _dadId, 
+        uint256 _generation,
+        uint256 _genes, 
+        address _owner) private returns (uint256) {
+            Kitty memory _kitty = Kitty({
+                genes: _genes,
+                birthTime: uint64(now),
+                mumId: uint32(_mumId),
+                dadId: uint32(_dadId),
+                generation: uint16(_generation)
+            });
+            kitties.push(_kitty);
+            uint newKittenId = kitties.length - 1;
+            _transfer(address(0), _owner, newKittenId);
+
+            emit Birth(_owner, newKittenId, _mumId, _dadId, _genes);
+
+            return newKittenId;
+        }
+
+
     /**
      * @dev Returns the number of tokens in ``owner``'s account.
      */
